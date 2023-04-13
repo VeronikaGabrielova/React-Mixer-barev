adfba1a3b9ab4b39b29ddce12933f0a064731341
VeronikaGabrielova <gabriel.veru@gmail.com>
2023-04-13 08:04:01 +0200

    useState(0),onValuChange

--- src/components/ColorSlider/index.jsx
+++ src/components/ColorSlider/index.jsx
@@ -2,22 +2,19 @@ import React from "react";
 import "./style.css";
 
 const ColorSlider = ({ baseColor, colorName, onValueChange }) => {
-  const handleValueChange = (event) => {
-    const value = event.target.value;
-    onValueChange(value);
-  };
   return (
     <>
       <label>{colorName}</label>
       <input
         type="range"
         className={`slider slider--${colorName}`}
-        id={`${colorName}Slider`}
         min="0"
         max="255"
         defaultValue="0"
         style={{ backgroundColor: `${baseColor}` }}
-        onChange={handleValueChange}
+        onChange={(event) => {
+          onValueChange(event.target.value);
+        }}
       />
     </>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,9 @@ import "./style.css";
 import ColorSlider from "./components/ColorSlider";
 
 const App = () => {
-  const [redValue, setRedValue] = useState("0");
-  const [greenValue, setGreenValue] = useState("0");
-  const [blueValue, setBlueValue] = useState("0");
+  const [redValue, setRedValue] = useState(0);
+  const [greenValue, setGreenValue] = useState(0);
+  const [blueValue, setBlueValue] = useState(0);
 
   const color = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
   return (
